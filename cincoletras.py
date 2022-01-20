64aefae7f661c3be8de92b44081941c979584372
Eric Moretti <ericmoretti100@gmail.com>
2022-01-20 20:40:47 -0300

    programa funcionando sem sistema das letras amarelas

--- cincoletras.py
+++ cincoletras.py
@@ -1,34 +1,53 @@
-import os
-os.system('cls' if os.name == 'nt' else 'clear')
+def imprimirdb():
+    print(leitura[1],leitura[2],leitura[3])
+    print('DB: ',len(leitura),'. Deseja imprimir?')
+    
+    quero = input()
+    if quero == 's':
+        x=0
+        while quero == 's':
+            for i in range (0,10):
+                if (i+x) > len(leitura):
+                    break
+                print (leitura[i+x])
+            x=x+10
+            print ('Deseja imprimir mais?',x,'/',len(leitura))
+            quero = input ()
 
-print("ola mundo")
+        
 #sgb-words
-with open("teste.txt","r") as arquivo:
+with open("sgb-words.txt","r") as arquivo:
 	leitura = arquivo.read().split()
-print (leitura[1])
 print ("dbsize = ", len(leitura))
 entrada = 'nada'
 
-for i in range(len(leitura)):
-    print(leitura[i])
-
 while entrada != 'sair':
-    posicao = input('digite a posicao\n')
-    posint = int(posicao)
-    caractere = input('digite o caractere\n')
-    posicoes = []
-    for x in range(len(leitura)):
-        if leitura[x][posint] != caractere:
-            posicoes.insert(0,x)
-    print (posicoes,len(posicoes))
+    entrada = input('Digite a posição ou a letra a ser bloqueada\n')
+    try:
+        posicao = int(entrada)
+        if posicao >= 0 and posicao<= 4:
+            caractere = input('digite o caractere\n')
+            posicoes = []
+            for x in range(len(leitura)):
+                if leitura[x][posicao] != caractere:
+                    posicoes.insert(0,x)
+        
+            for x in range(len(posicoes)):
+                remover = posicoes[x]
+                leitura.pop(remover)
+            imprimirdb()
+    except ValueError:  
+        posicoes = []
+        for x in range(len(leitura)):
+            for i in range(0,5):
+                if leitura[x][i] == entrada:
+                    posicoes.insert(0,x)
+                    break
+        #print (posicoes)
     
-    for x in range(len(posicoes)):
-        print(x,posicoes[x])
-        remover = posicoes[x]
-        leitura.pop(remover)
-    print(leitura)
-                                       
-#for i in len(leitura)
+        for x in range(len(posicoes)):
+            #print(x,posicoes[x])
+            remover = posicoes[x]
+            leitura.pop(remover)
+        imprimirdb()
 
-print ("deu bom",entrada)
-	
