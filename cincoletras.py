9ce04f5876120122adc7c0b08b1d00f24c3bb5f5
Eric Moretti <ericmoretti100@gmail.com>
2022-01-20 21:18:34 -0300

    organizando o programa em funções

--- cincoletras.py
+++ cincoletras.py
@@ -1,3 +1,6 @@
+#fazer a lista de caracteres banidos para ver se n da bosta
+
+
 def imprimirdb():
     print(leitura[0])
     print('DB: ',len(leitura),'. Deseja imprimir?')
@@ -17,40 +20,72 @@ def imprimirdb():
                 print ('Deseja imprimir mais?',x,'/',len(leitura))
                 quero = input ()
 
+
+def remover_pretas(letra):
+    posicoes = []
+    for x in range(len(leitura)):
+        for i in range(0,5):
+            if leitura[x][i] == letra:
+                posicoes.insert(0,x)
+                break
+            
+    for x in range(len(posicoes)):
+        remover = posicoes[x]
+        leitura.pop(remover)
+
+                   
+def salvar_verde(pos,letra):
+    posicoes = []
+    for x in range(len(leitura)):
+        if leitura[x][pos] != letra:
+            posicoes.insert(0,x)
+    
+    for x in range(len(posicoes)):
+        remover = posicoes[x]
+        leitura.pop(remover)
+        
+#def salvar_amarela(letra)
         
 #sgb-words
 with open("sgb-words.txt","r") as arquivo:
 	leitura = arquivo.read().split()
 print ("dbsize = ", len(leitura))
 entrada = 'nada'
+"""
+Entradas:
 
-while entrada != 'sair':
-    entrada = input('Digite a posição ou a letra a ser bloqueada\n')
-    try:
-        posicao = int(entrada)
-        if posicao >= 0 and posicao<= 4:
-            caractere = input('digite o caractere\n')
-            posicoes = []
-            for x in range(len(leitura)):
-                if leitura[x][posicao] != caractere:
-                    posicoes.insert(0,x)
-        
-            for x in range(len(posicoes)):
-                remover = posicoes[x]
-                leitura.pop(remover)
-            imprimirdb()
-    except ValueError:  
-        posicoes = []
-        for x in range(len(leitura)):
-            for i in range(0,5):
-                if leitura[x][i] == entrada:
-                    posicoes.insert(0,x)
-                    break
-        #print (posicoes)
-    
-        for x in range(len(posicoes)):
-            #print(x,posicoes[x])
-            remover = posicoes[x]
-            leitura.pop(remover)
+am v
+vd 3 a
+pt l
+"""
+while entrada != 'sair':    
+    entrada = input('Digite o comando: am [letra] / vd [pos] [letra] / pt [letra] \n')
+    entrada_limpa = entrada.split()
+    if entrada_limpa[0] == 'am':
+        #amarelo
+        print('amarelow')
+    elif entrada_limpa[0] == 'vd':
+        #verde
+        salvar_verde(entrada_limpa[1],entrada_limpa[2])
         imprimirdb()
-
+    elif entrada_limpa[0] == 'pt':
+        #preto
+        remover_pretas(entrada_limpa[1])
+        imprimirdb()
+    else:
+        print ('Entrada Inválida, tente novamente.\n')
+    """
+    try:
+            posicao = int(entrada)
+            if posicao >= 0 and posicao<= 4:
+                caractere = input('digite o caractere\n')
+                salvar_verde(posicao,caractere)
+                
+                imprimirdb()
+    except ValueError:
+            remover_pretas(entrada)            
+            imprimirdb()
+    else:
+        entrada.split()
+        
+"""
